1502379629576d1fd96271edb50b497c6e448651
BLW-Code <blwilson2003@gmail.com>
2025-07-11 20:23:44 -0500

    updated file path again

--- projectArchive/build.sh
+++ projectArchive/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r /projectArchive/requirements.txt
+pip install -r ./projectArchive/requirements.txt
 
 python manage.py collectstatic --no-input
 
