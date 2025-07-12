184c7c63bea4c34871cb7e09211d8c33d9cf2666
BLW-Code <blwilson2003@gmail.com>
2025-07-11 20:55:09 -0500

    initial database setup

--- projectArchive/projectManager/admin.py
+++ projectArchive/projectManager/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Person, Webpage, Project
 
-# Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+
+@admin.register(Webpage)
+class WebpageAdmin(admin.ModelAdmin):
+    list_display = ('url',)
+    search_fields = ('url',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
+    search_fields = ('name',)
+    list_filter = ('start_date', 'end_date')
+    filter_horizontal = ('workers', 'approvers', 'webpages')  # Makes multi-select UI cleaner
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'start_date', 'end_date')
+        }),
+        ('People Involved', {
+            'fields': ('workers', 'approvers'),
+        }),
+        ('Project Webpages', {
+            'fields': ('webpages',),
+        }),
+        ('Documents', {
+            'fields': ('email_pdf', 'comparison_pdf'),
+        }),
+    )
--- projectArchive/projectManager/models.py
+++ projectArchive/projectManager/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
+
+class Webpage(models.Model):
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.url
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    workers = models.ManyToManyField(Person, related_name='projects_worked_on')
+    approvers = models.ManyToManyField(Person, related_name='projects_approved')
+    webpages = models.ManyToManyField(Webpage, related_name='projects')
+
+    email_pdf = models.FileField(upload_to='project_pdfs/email_pdfs/')
+    comparison_pdf = models.FileField(upload_to='project_pdfs/comparison_pdfs/')
+
+    def __str__(self):
+        return self.name
